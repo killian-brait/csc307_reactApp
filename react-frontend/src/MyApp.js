0dbecebd96c99027a4d303a83ed57f7ef2721690
Killian Brait <killianbrait@gmail.com>
2023-04-10 19:24:06 -0700

    stateful elements

--- react-frontend/src/MyApp.js
+++ react-frontend/src/MyApp.js
@@ -1,33 +1,39 @@
-import React from 'react'
-import Table from './Table'
+import React, {useState} from 'react';
+import Table from './Table';
 
 
-
-const characters = [
+function MyApp() {
+  const [characters, setCharacters] = useState([
     {
       name: 'Charlie',
       job: 'Janitor',
     },
     {
-      name: 'Mac',
-      job: 'Bouncer',
-    },
-    {
       name: 'Dee',
-      job: 'Aspring actress',
+      job: 'Aspiring Actress',
     },
     {
       name: 'Dennis',
       job: 'Bartender',
     },
-];
+    {
+      name: 'Mac',
+      job: 'Bouncer'
+    }
+  ]);
 
-function MyApp() {
+  function removeOneCharacter (index) {
+    const updated = characters.filter((character, i) => {
+      return i !== index
+    });
+    setCharacters(updated);
+  }
+
   return (
     <div className="container">
-      <Table characterData={characters} />
+      <Table characterData={characters} removeCharacter={removeOneCharacter} />
     </div>
-  );  
+  )
 }
 
 export default MyApp;
