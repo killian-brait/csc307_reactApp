f957b3304b757b370e75b9e1c2704921579b588f
Killian Brait <killianbrait@gmail.com>
2023-04-11 15:12:40 -0700

    DELETE /users/:id

--- expressjs-backend/backend.js
+++ expressjs-backend/backend.js
@@ -57,7 +57,7 @@ app.get('/users', (req, res) => {
 app.get('/users/:id', (req, res) => {
     const id = req.params.id;
     let result = findUserById(id);
-    if (result === undefined || result.length == 0)
+    if (result === undefined || result.length === 0)
         res.status(404).send('Resource not found.');
     else {
         result = {users_list: result};
@@ -78,9 +78,26 @@ app.post('/users', (req, res) => {
     // }
 });
 
+// delete a user by id
+app.delete('/users/:id', (req, res) => {
+  const id = req.params.id;
+  let userToRemove = findUserById(id);
+  if (userToRemove === undefined || userToRemove.length === 0)
+    res.status(404).send("Resource not found.");
+  else {
+    removeUser(userToRemove);
+    res.status(200).end();
+  }
+});
+
 // helper functions
 function addUser(user) {
     users['users_list'].push(user);
+}
+
+function removeUser(user) {
+  const index = users['users_list'].indexOf(user);
+  users['users_list'].splice(index, 1);
 }
 
 function validateData(user) {
