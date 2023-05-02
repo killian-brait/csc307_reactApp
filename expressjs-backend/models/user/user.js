bc3f1de62d3adc635b64c5e2a3c3779fd570e662
Killian Brait <killianbrait@gmail.com>
2023-05-02 13:09:50 -0700

    user_model_services

--- expressjs-backend/models/user/user.js
+++ expressjs-backend/models/user/user.js
@@ -1,2 +1,28 @@
 const mongoose = require("mongoose");
 
+const UserSchema = new mongoose.Schema(
+    {
+        name: {
+            type: String,
+            required: true,
+            trim: true,
+        },
+        job: {
+            type: String,
+            required: true,
+            trim: true,
+            // Q: what does value refer to in the following code?
+            // A: value refers to the value of the job field
+            validate(value) {
+                if (value.length < 2) {
+                    throw new Error("Invalid job, must be at least 2 characters.");
+                }
+            }
+        }
+    }, 
+    { collection: "users_list" } 
+);
+
+const User = mongoose.model("User", UserSchema);
+
+module.exports = User;
---
+++ expressjs-backend/models/user/userServices.js
@@ -0,0 +1,72 @@
+const mongoose = require('mongoose');
+const userModel = require('./user');
+mongoose.set("debug", true);
+
+mongoose
+    .connect("mongodb://localhost:27017/users", {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .catch((error) => console.log(error));
+
+// get users by name or job or both
+async function getUsers(name, job) {
+    let result;
+    if (name === undefined && job === undefined) {
+        result = await userModel.find();
+    } else if (name && !job) {
+        result = await findUserByName(name);
+    } else if (!name && job) {
+        result = await findUserByJob(job);
+    } else if (name && job) {
+        result = await findUserByNameJob(name, job);
+    }
+    return result;
+}
+
+// find a user by id
+async function findUserById(id) {
+    try {
+        return await userModel.findById(id);
+    } catch (error) {
+        console.log(error);
+        return undefined;
+    }
+}
+
+// add a user
+async function addUser(user) {
+    try {
+        const userToAdd = new userModel(user);
+        const savedUser = await userToAdd.save();
+        return savedUser;
+    } catch (error) {
+        console.log(error);
+        return false;
+    }
+}
+
+
+// --------------------------------------------------
+// HELPER FUNCTIONS
+// --------------------------------------------------
+async function findUserByName(name) {
+    return await userModel.find({name: name});
+}
+
+async function findUserByJob(job) {
+    return await userModel.find({job: job});
+}
+
+async function findUserByNameJob(name, job) {
+    return await userModel.find({name: name, job: job});
+}
+
+
+// --------------------------------------------------
+// EXPORTS
+// --------------------------------------------------
+exports.getUsers = getUsers;
+exports.findUserById = findUserById;
+exports.addUser = addUser;
+
